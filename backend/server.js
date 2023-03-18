a746aa2c0da624ac65d6cf0511a17bacdf8774a3
shubham999999 <shubhammote999999@gmail.com>
2023-03-18 20:22:54 +0530

    Revert "Update server.js"
    This reverts commit 891c7ce4c1e6c663454d289a555da5eea6132946.

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,10 @@ app.use("/api/message", messageRoutes);
 /******************* Deployment *******************/
 
 const __dirname1 = path.resolve();
-console.log(__dirname1);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "frontend/build")));
-  console.log("stuck here");
-  
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
